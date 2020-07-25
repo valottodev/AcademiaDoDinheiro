3c0dfb76b9aaec40a3d1632944a1b34e7650fceb
Hendy <hendy.almeida@hotmail.com>
2020-07-25 18:35:22 -0300

    fix header, footer e home

---
+++ footer.php
@@ -0,0 +1,88 @@
+<footer>
+  <div class="wrapper container">
+    <div class="row">
+      <div class="col-md-4">
+        <div class="d-flex justify-content-between align-items-center flex-wrap">
+          <h2>Vamos falar</h2>
+          <div class="link-social">
+          </div>
+        </div>
+
+        <form class="contato">
+          <div class="input-group input-group-sm mb-3">
+            <div class="input-group-prepend">
+              <span class="input-group-text" id="inputGroup-sizing-sm"><img src="assets/Icon-user.svg" alt="icone usuário"></span>
+            </div>
+            <input type="text" class="form-control" placeholder="Seu nome" aria-describedby="inputGroup-sizing-sm" />
+          </div>
+
+          <div class="input-group input-group-sm mb-3">
+            <div class="input-group-prepend">
+              <span class="input-group-text" id="inputGroup-sizing-sm">
+                <img src="assets/Icon-phone.svg" alt="icone telefone">
+              </span>
+            </div>
+            <input type="text" class="form-control" placeholder="Telefone" aria-describedby="inputGroup-sizing-sm" />
+          </div>
+
+          <div class="input-group input-group-sm mb-3">
+            <div class="input-group-prepend">
+              <span class="input-group-text" id="inputGroup-sizing-sm">
+                <img src="assets/icon-mail.svg" alt="icone email">
+              </span>
+            </div>
+            <input type="text" class="form-control" placeholder="E-mail" aria-describedby="inputGroup-sizing-sm" />
+          </div>
+
+          <div class="input-group input-group-sm mb-3 mt-5">
+            <div class="input-group-prepend">
+              <span class="input-group-text" id="inputGroup-sizing-sm">
+                <img src="assets/icon-menu.svg" alt="icone email">
+              </span>
+            </div>
+            <input type="text" class="form-control" placeholder="Mensagem" aria-describedby="inputGroup-sizing-sm" />
+          </div>
+          <div class="my-5 text-right">
+            <button class="btn btn-primry">Enviar</button>
+          </div>
+        </form>
+      </div>
+
+      <div class="navigation col-md-4 offset-md-1">
+        <h3>Site</h3>
+        <nav>
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Dicas para investir</a></li>
+            <li><a href="#">Renda variável</a></li>
+            <li><a href="#">Renda fixa</a></li>
+            <li><a href="#">Recursos</a></li>
+            <li><a href="#">Treinamentos</a></li>
+            <li><a href="#">Clínica financeira</a></li>
+            <li><a href="#">A Academia</a></li>
+          </ul>
+        </nav>
+
+        <h3>Treinamentos</h3>
+        <nav>
+          <ul>
+            <li><a href="#">Método Fast de Enriquecimento</a></li>
+            <li><a href="#">Aposentadoria e renda com fundos imobiliários</a></li>
+            <li><a href="#">Gestão vencedora de finanças para negócios</a></li>
+            <li><a href="#">Finanças turbinadas</a></li>
+          </ul>
+        </nav>
+      </div>
+
+      <div class="logo col-md-3">
+        <img class="logo img-fluid" src="assets/logo-white.svg" alt="Academia do Dinheiro" />
+      </div>
+    </div>
+  </div>
+</footer>
+<script src="scripts/jquery.js" defer></script>
+<script src="scripts/popper.js" defer></script>
+<script src="scripts/bootstrap.js" defer></script>
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no" />
+  <title>Academia do Dinheiro</title>
+  <link rel="stylesheet" href="styles/compiler/bootstrap.css" />
+  <link rel="stylesheet" href="styles/compiler/style.css" />
+  <link rel="stylesheet" href="styles/main.css" />
+  <link rel="stylesheet" href="styles/index.css" />
+</head>
+
+<body>
+  <header class="d-flex flex-column">
+    <div class="d-flex container d-flex justify-content-between flex-wrap">
+      <img class="logo" src="assets/logo.svg" alt="Academia do Dinheiro" />
+      <div class="container-anuncio d-flex justify-content-center align-items-center"><span>Anuncie aqui</span></div>
+    </div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSite">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSite">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a href="index.html" class="nav-link active">A academia</a>
+            </li>
+            <li class="nav-item">
+              <a href="recursos.html" class="nav-link">Recursos</a>
+            </li>
+            <li class="nav-item">
+              <a href="treinamento.html" class="nav-link">Treinamento</a>
+            </li>
+            <li class="nav-item">
+              <a href="clinica.html" class="nav-link">Clínica financeira</a>
+            </li>
+            <li class="nav-item">
+              <a href="blog.html" class="nav-link">Blog</a>
+            </li>
+          </ul>
+        </div>
+        <div class="social-media">
+          <a href="https://www.youtube.com/user/profmaurocalil" target="_blank"><img src="assets/youtube.png" alt="youtube" class="icon" /></a>
+          <a href="https://www.instagram.com/academiadodinheiro/" target="_blank"><img src="assets/instagram-sketched.png" alt="instagram" class="icon" /></a>
+          <a href="/search"><img src="assets/search.png" alt="busca" class="icon search" /></a>
+        </div>
+      </div>
+    </nav>
+  </header>
