b2354a1270791c67bb2e7085a17713a9be7c5feb
Lucas Valotto <lucas.valotto@gmail.com>
2020-07-26 19:52:35 -0300

    ajustes

--- blog-post.php
+++ blog-post.php
@@ -2,36 +2,42 @@
 <main class="blog">
   <section>
     <div class="container-fluid">
-      <div class="img-head">
-      </div>
-
-      <div class="col-md-8 conteudo">
-        <div class="redes">
-          <a href="https://www.youtube.com/user/profmaurocalil" target="_blank">
-            <img src="assets/Facebook.svg" alt="youtube" class="icon" /></a>
-          <a href="https://www.instagram.com/academiadodinheiro/" target="_blank">
-            <img src="assets/LinKeDin.svg" alt="instagram" class="icon" />
-          </a>
-          <a href="/search">
-            <img src="assets/Twitter.svg" alt="busca" class="icon search" />
-          </a>
+      <div class="row">
+        <div class="col img-head">
         </div>
-
-        <div class="texto-post">
-          <h1>Título em Roboto Black 70pt e BG 85% com quebra de linha com espaçamento de 60</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat molestiae illo laudantium veniam, voluptatem amet soluta quibusdam? Molestiae reiciendis laborum voluptatem perferendis, saepe autem veritatis praesentium, magnam et minima iste.</p>
-          <div class="img-post">
-            <img src="assets/man-in-black-holding-phone-618613.svg" alt="">
+      </div>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-9 conteudo">
+          <div class="redes">
+            <a href="https://www.youtube.com/user/profmaurocalil" target="_blank">
+              <img src="assets/Facebook.svg" alt="youtube" class="icon" /></a>
+            <a href="https://www.instagram.com/academiadodinheiro/" target="_blank">
+              <img src="assets/LinKeDin.svg" alt="instagram" class="icon" />
+            </a>
+            <a href="/search">
+              <img src="assets/Twitter.svg" alt="busca" class="icon search" />
+            </a>
           </div>
+  
+          <div class="texto-post">
+            <h1>Título em Roboto Black 70pt e BG 85% com quebra de linha com espaçamento de 60</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat molestiae illo laudantium veniam, voluptatem amet soluta quibusdam? Molestiae reiciendis laborum voluptatem perferendis, saepe autem veritatis praesentium, magnam et minima iste.</p>
+            <div class="img-post">
+              <img src="assets/man-in-black-holding-phone-618613.svg" alt="">
+            </div>
+          </div>
+  
+  
         </div>
-
 
       </div>
 
 
 
 
-      <div class="col-md-4 lateral">
+      <div class="col-md-3 lateral">
 
       </div>
 
