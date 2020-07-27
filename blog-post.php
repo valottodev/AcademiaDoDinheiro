bd988d1a728320f9dbf553dee43f1dcca541fb49
Lucas Valotto <lucas.valotto@gmail.com>
2020-07-27 00:46:03 -0300

    blog-post finalizada

--- blog-post.php
+++ blog-post.php
@@ -2,8 +2,8 @@
 <main class="blog">
   <section>
     <div class="container-fluid">
-      <div class="row">
-        <div class="col img-head">
+      <div class="row d-flex justify-content-center">
+        <div class="col-10 img-head">
         </div>
       </div>
     </div>
@@ -20,26 +20,124 @@
               <img src="assets/Twitter.svg" alt="busca" class="icon search" />
             </a>
           </div>
-  
+        
           <div class="texto-post">
             <h1>Título em Roboto Black 70pt e BG 85% com quebra de linha com espaçamento de 60</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat molestiae illo laudantium veniam, voluptatem amet soluta quibusdam? Molestiae reiciendis laborum voluptatem perferendis, saepe autem veritatis praesentium, magnam et minima iste.</p>
-            <div class="img-post">
-              <img src="assets/man-in-black-holding-phone-618613.svg" alt="">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consequat ac felis donec et odio pellentesque diam volutpat. Donec et odio pellentesque diam volutpat commodo. Dui accumsan sit amet nulla facilisi morbi tempus iaculis urna. Ut porttitor leo a diam sollicitudin tempor. Sodales ut etiam sit amet nisl. Nulla facilisi nullam vehicula ipsum a arcu cursus. Amet purus gravida quis blandit turpis. Facilisi morbi tempus iaculis urna id volutpat lacus. Tempus quam pellentesque nec nam. Enim praesent elementum facilisis leo vel fringilla est. Diam maecenas ultricies mi eget mauris pharetra. Semper quis lectus nulla at volutpat. In nisl nisi scelerisque eu ultrices vitae auctor eu augue. Est ultricies integer quis auctor elit. Justo eget magna fermentum iaculis eu. Tincidunt vitae semper quis lectus nulla at volutpat diam ut. Amet purus gravida quis blandit turpis.</p>
+            <div class="img-post" style="background-image: url(assets/man-in-black-holding-phone-618613.png);"></div>
+            <p>Justo eget magna fermentum iaculis eu non diam phasellus vestibulum. At quis risus sed vulputate odio ut enim blandit. Amet dictum sit amet justo donec enim diam. Lorem ipsum dolor sit amet consectetur adipiscing. Augue mauris augue neque gravida in fermentum et sollicitudin ac. Sapien faucibus et molestie ac feugiat sed lectus vestibulum.</p>
+            <h2>Subtítulo</h2>
+            <p>Imperdiet nulla malesuada pellentesque elit eget gravida cum sociis natoque. Sed nisi lacus sed viverra tellus in hac habitasse. Condimentum id venenatis a condimentum vitae. Non diam phasellus vestibulum lorem sed risus. Enim lobortis scelerisque fermentum dui. Id nibh tortor id aliquet. Ultricies lacus sed turpis tincidunt id aliquet. Tempus quam pellentesque nec nam aliquam sem.</p>
+            <p>Adipiscing tristique risus nec feugiat in fermentum. Morbi tristique senectus et netus et malesuada fames. Orci eu lobortis elementum nibh tellus. Quam nulla porttitor massa id neque aliquam vestibulum morbi blandit. Sit amet risus nullam eget felis. Varius morbi enim nunc faucibus a. A erat nam at lectus. Augue eget arcu dictum varius duis at consectetur. Lacus vel facilisis volutpat est velit egestas dui id ornare. Nullam vehicula ipsum a arcu cursus vitae congue mauris. Ut sem viverra aliquet eget sit amet tellus cras adipiscing. Porta nibh venenatis cras sed. Parturient montes nascetur ridiculus mus mauris vitae. Sed viverra tellus in hac habitasse.</p>
+          </div>
+          <div class="relacionados">
+            <p class="btn btn-secondary">Artigos Relacionados</p>
+            <div class="row">
+              <div class="col-md-3">
+                <article>
+                  <a href="curso.php">
+                    <div class="thumb" style="background-image: url(assets/relacionados.png);"></div>
+                    <div class="description" style="color: #3C6B6A;">
+                      <h4>Clique nesse curso para ver um exemplo</h4>
+                    </div>
+                  </a>
+                </article>
+              </div> 
+              <div class="col-md-3">
+                <article>
+                  <a href="curso.php">
+                    <div class="thumb" style="background-image: url(assets/relacionados.png);"></div>
+                    <div class="description" style="color: #3C6B6A;">
+                      <h4>Clique nesse curso para ver um exemplo</h4>
+                    </div>
+                  </a>
+                </article>
+              </div> 
+              <div class="col-md-3">
+                <article>
+                  <a href="curso.php">
+                    <div class="thumb" style="background-image: url(assets/relacionados.png);"></div>
+                    <div class="description" style="color: #3C6B6A;">
+                      <h4>Clique nesse curso para ver um exemplo</h4>
+                    </div>
+                  </a>
+                </article>
+              </div> 
+              <div class="col-md-3">
+                <article>
+                  <a href="curso.php">
+                    <div class="thumb" style="background-image: url(assets/relacionados.png);"></div>
+                    <div class="description" style="color: #3C6B6A;">
+                      <h4>Clique nesse curso para ver um exemplo</h4>
+                    </div>
+                  </a>
+                </article>
+              </div> 
             </div>
           </div>
-  
-  
+
+
         </div>
 
+        <div class="col-md-3 lateral">
+
+          <form class="newsletter">
+            <p>Newsletter | Inscreva-se</p>
+            <div class="input-group input-group-sm mb-3">
+              <div class="input-group-prepend">
+                <span class="input-group-text" id="inputGroup-sizing-sm"><img src="assets/Icon-mail.svg" alt="icone usuário"></span>
+              </div>
+              <input type="text" class="form-control" placeholder="E-mail" aria-describedby="inputGroup-sizing-sm" id="mailNewsletter"/>
+            </div>
+
+            <button class="btn btn-primary">Enviar</button>
+            
+          </form>
+
+          <div class="anuncio-lateral" style="height: 250px; width: 100%; border: 1px solid #707070;"></div>
+          <div class="mais-lidos">
+            <p class="tag-light">Mais lidos</p>
+            
+            <article>
+              <a href="curso.php">
+                <div class="thumb" style="background-image: url(assets/relacionados.png);"></div>
+                <div class="description" style="color: #3C6B6A;">
+                  <h4>Clique nesse curso para ver um exemplo</h4>
+                </div>
+              </a>
+            </article>
+            
+            <article>
+              <a href="curso.php">
+                <div class="thumb" style="background-image: url(assets/maislidos1.png);"></div>
+                <div class="description" style="color: #3C6B6A;">
+                  <h4>Clique nesse curso para ver um exemplo</h4>
+                </div>
+              </a>
+            </article>
+            <article>
+              <a href="curso.php">
+                <div class="thumb" style="background-image: url(assets/maislidos2.png);"></div>
+                <div class="description" style="color: #3C6B6A;">
+                  <h4>Clique nesse curso para ver um exemplo</h4>
+                </div>
+              </a>
+            </article>
+            <article>
+              <a href="curso.php">
+                <div class="thumb" style="background-image: url(assets/maislidos3.png);"></div>
+                <div class="description" style="color: #3C6B6A;">
+                  <h4>Clique nesse curso para ver um exemplo</h4>
+                </div>
+              </a>
+            </article>
+          </div>
+          <div class="anuncio-lateral" style="height: 250px; width: 100%; border: 1px solid #707070;"></div>
+          
+        </div>
       </div>
 
-
-
-
-      <div class="col-md-3 lateral">
-
-      </div>
+      
 
 
 
